a9738275edf09d26da1db8c5013fcc15c2b14f7a
imran4ik <110815719+imran4ik@users.noreply.github.com>
2022-09-20 11:50:42 +0400

    Add files via upload

---
+++ java_script/js/script.js
@@ -0,0 +1,60 @@
+// let num1 = 10;
+// let num2 = 20;
+// let num3 = 30;
+// let num4 = 40;
+
+// if (num1 != num2 || num3 >= num4){
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// num2 = 200;
+
+// if (num2 < num4){
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// console.log(num2);
+
+// let num = 15;
+
+// for (let num = 0; num < 15; i++) {
+//     text += i + "<br>";
+//   }
+
+// let num2 = 153;
+
+// let b = 0;
+
+
+// for (let i = 0; i < 7; i++) {
+//     (num2 / i);
+//     console.log(num2);
+//   }
+
+console.log(15 % 2);
+
+let num = 15;
+
+let a = 0;
+
+
+for (let a = 0; a < 7; a++) {
+    num % a;
+    console.log(num);
+  }
+
+for (let b = 2; b <= 15; b++) {
+    if (b % 2 == 0) {
+        console.log( b );
+    }
+  }
+
+for (let c = 2; c <= 10; c++) {
+    if (c % 2 == 0) {
+      console.log( c );
+    }
+  }
